5f04c8c2611b961eab8b3be2bad1b603850e8f7f
Alex Fernando <alexfernand.o@outlook.com>
2024-05-24 01:05:05 -0300

    update tab1 html and tab1 ts files

--- GRP05/pokemon/src/app/tab1/tab1.page.ts
+++ GRP05/pokemon/src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PokeAPIService } from '../services/poke-api.service';
+import { ViaCEPService } from '../services/via-cep.service';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  areaBuscarPokemon: string = '52011210';
+  areaBusca: any ={
+    bairro: '',
+    localidade: '',
+    logradouro: '',
+    uf: ''
+  }
 
-  constructor() {}
+  pokemon: any = {
+    name: '',
+    front_default: '',
+    abilities: '',
+    height: '',
+    weight: ''
+  }
 
+  constructor(
+    private pokeAPIService: PokeAPIService,
+    private viaCEPService: ViaCEPService
+  ) {}
+  buscarPokemon(){
+    this.viaCEPService.getViaCEPService(this.areaBuscarPokemon).subscribe((value)=>{
+      this.areaBusca.logradouro = JSON.parse(JSON.stringify(value))['logradouro'];
+      this.areaBusca.bairro = ', ' + JSON.parse(JSON.stringify(value))["bairro"];
+      this.areaBusca.localidade = ' - ' + JSON.parse(JSON.stringify(value))['localidade'];
+      this.areaBusca.uf = '-'+ JSON.parse(JSON.stringify(value))['uf'];
+    });
+    this.pokeAPIService.getPokeAPIService().subscribe((value)=>{
+      this.pokemon.name = JSON.parse(JSON.stringify(value))['name'];
+      this.pokemon.front_default = JSON.parse(JSON.stringify(value))['sprites']['other']['dream_world']['front_default'];
+      this.pokemon.abilities = JSON.parse(JSON.stringify(value))['abilities'].length;
+      this.pokemon.height = JSON.parse(JSON.stringify(value))['height'];
+      this.pokemon.weight = JSON.parse(JSON.stringify(value))['weight'];
+    });
+  }
 }
