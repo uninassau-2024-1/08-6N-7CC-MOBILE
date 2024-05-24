9b1fbe436c529383049a239e86fca4f973e9c91b
Alex Fernando <alexfernand.o@outlook.com>
2024-05-24 12:32:28 -0300

    update App.module.ts

--- GRP05/pokemon/src/app/app.module.ts
+++ GRP05/pokemon/src/app/app.module.ts
@@ -8,10 +8,18 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Tab1Page } from './tab1/tab1.page';
+import { Tab2Page } from './tab2/tab2.page';
+import { PokeAPIService } from './services/poke-api.service';
+import { ViaCEPService } from './services/via-cep.service';
+import { SharedService } from './services/shared.service';
+
+
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, PokeAPIService, ViaCEPService, SharedService],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
