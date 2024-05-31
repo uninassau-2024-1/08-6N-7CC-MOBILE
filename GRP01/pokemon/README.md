204d4fccdf27a4b1ff4078d6a29f331979fdb0a3
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2024-05-30 21:22:57 -0300

    feat: Adiciona projeto do grupo 01
    Signed-off-by: LucasSalesMoreira <lucasdevsoftware@gmail.com>

--- GRP01/pokemon/README.md
+++ GRP01/pokemon/README.md
@@ -1,3 +1,7 @@
 # Alunos
 
-* Nome - Matrícula
+* Lucas Sales Moreira Cordeiro - 01351184
+* Alisson Lucas Almeida da Silva - 01431006
+* ⁠Pedro Paulo Campos - 01414565
+* Rafael tomas souza dos Santos - 01456056
+* ⁠Luiz Nelson da Silva Neto - 01421013
---
+++ GRP01/pokemon/src/app/tab1/tab1.page.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { PokeAPIService } from '../services/poke-api.service';
+import { ViaCEPService } from '../services/via-cep.service';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  areaBuscarPokemon: string = '';
+  areaBusca: any = {
+    bairro: '',
+    lacolidade: '',
+    logradouro: '',
+    uf: ''
+  };
+
+  constructor(
+    public pokeAPIService: PokeAPIService,
+    public viaCEPService: ViaCEPService,
+    private http: HttpClient
+  ) { }
+
+  buscarPokemon() {
+    this.viaCEPService.getViaCEPService(this.areaBuscarPokemon).subscribe((value) => {
+      this.areaBusca.logradouro = JSON.parse(JSON.stringify(value))['logradouro'];
+      this.areaBusca.bairro = ', ' + JSON.parse(JSON.stringify(value))['bairro'];
+      this.areaBusca.lacolidade = ' - ' + JSON.parse(JSON.stringify(value))['localidade'];
+      this.areaBusca.uf = '-' + JSON.parse(JSON.stringify(value))['uf'];
+    });
+    this.pokeAPIService.getPokeAPIService().subscribe((value) => {
+      this.pokeAPIService.pokemonMeu.abilities = JSON.parse(JSON.stringify(value))['abilities'].length;
+      this.pokeAPIService.pokemonMeu.front_default = JSON.parse(JSON.stringify(value))['sprites']['other']['dream_world']['front_default'];
+      this.pokeAPIService.pokemonMeu.height = JSON.parse(JSON.stringify(value))['height'];
+      this.pokeAPIService.pokemonMeu.name = JSON.parse(JSON.stringify(value))['name'];
+      this.pokeAPIService.pokemonMeu.weight = JSON.parse(JSON.stringify(value))['weight'];
+    });
+    this.pokeAPIService.pokemonPokedex.push(this.pokeAPIService.pokemonMeu);
+  }
+}
