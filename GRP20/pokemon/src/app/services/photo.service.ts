9030d34d2305cd6d34b11eef068f2f6310844c98
anajuliaflx <anajuliafdoliveira@gmail.com>
2024-05-31 21:22:10 -0300

    projeto grp20 pokemon

---
+++ GRP20/pokemon/src/app/services/photo.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Preferences } from '@capacitor/preferences';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  constructor() { }
+
+  public async addNewToGallery() {
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+  }
+}
---
+++ GRP20/pokemon/src/app/tab3/tab3.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { PokedexService } from '../services/pokedex.service';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page implements OnInit {
+  pokedex: any[] = [];
+
+  constructor(private pokedexService: PokedexService) {}
+
+  ngOnInit() {
+    this.loadPokedex();
+  }
+
+  ionViewWillEnter() {
+    this.loadPokedex();
+  }
+
+  loadPokedex() {
+    this.pokedex = this.pokedexService.getPokedex();
+  }
+}
